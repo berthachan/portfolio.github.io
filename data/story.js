445734bf5ccbe0a04ab4bd7d2a527d86b98bdd38
Bertha <bertha.clchan@gmail.com>
2024-12-16 21:06:10 -0500

    Add files via upload

---
+++ data/story.js
@@ -0,0 +1,470 @@
+var story = {
+ "docName": "Bertha's Portfolio",
+ "docPath": "P_P_P",
+ "docVersion": 100000001,
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "fileType": "png",
+ "disableInteractions": false,
+ "highlightHotspot": false,
+ "highlightAllHotspots": false,
+ "hideGallery": true,
+ "galleryPageColorsEnabled": false,
+ "galleryDecorNodesEnabled": false,
+ "zoomEnabled": true,
+ "cloud": false,
+ "singleFile": false,
+ "title": "Bertha's Portfolio",
+ "layersExist": false,
+ "pages": [
+  {
+   "id": "404:91",
+   "groupIndex": 0,
+   "title": "Portfolio Home Page",
+   "image": "portfolio-home-page.png",
+   "index": 0,
+   "width": 1440,
+   "height": 1682,
+   "x": 598,
+   "y": -931,
+   "awidth": 1440,
+   "aheight": 1682,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Home",
+     "rect": {
+      "x": 1184,
+      "y": 1592,
+      "width": 96,
+      "height": 20
+     },
+     "index": 0,
+     "reactions": [
+      {
+       "action": "URL",
+       "srcPageIndex": 0,
+       "url": "https://www.linkedin.com/in/berthaclchan/",
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "IMT 1",
+     "rect": {
+      "x": 160,
+      "y": 1021,
+      "width": 544,
+      "height": 272
+     },
+     "index": 1,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 0,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Frame 42362",
+     "rect": {
+      "x": 736,
+      "y": 1021,
+      "width": 544,
+      "height": 424
+     },
+     "index": 2,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 0,
+       "frameIndex": 3,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Frame 6",
+     "rect": {
+      "x": 1183.82373046875,
+      "y": 83,
+      "width": 96,
+      "height": 20
+     },
+     "index": 3,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 0,
+       "frameIndex": 1,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "404:135",
+   "groupIndex": 0,
+   "title": "Portfolio About Page",
+   "image": "portfolio-about-page.png",
+   "index": 1,
+   "width": 1440,
+   "height": 1351,
+   "x": 2116,
+   "y": -377,
+   "awidth": 1440,
+   "aheight": 1351,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Footer Home",
+     "rect": {
+      "x": 160,
+      "y": 1279,
+      "width": 270.82373046875,
+      "height": 20
+     },
+     "index": 4,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Home",
+     "rect": {
+      "x": 1184,
+      "y": 1279,
+      "width": 96,
+      "height": 20
+     },
+     "index": 5,
+     "reactions": [
+      {
+       "action": "URL",
+       "srcPageIndex": 1,
+       "url": "https://www.linkedin.com/in/berthaclchan/",
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Home",
+     "rect": {
+      "x": 162,
+      "y": 83,
+      "width": 270.82373046875,
+      "height": 20
+     },
+     "index": 6,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "404:173",
+   "groupIndex": 0,
+   "title": "IMT",
+   "image": "imt.png",
+   "index": 2,
+   "width": 1440,
+   "height": 10591,
+   "x": 2116,
+   "y": 1177,
+   "awidth": 1440,
+   "aheight": 10591,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Home",
+     "rect": {
+      "x": 160,
+      "y": 83,
+      "width": 270.82373046875,
+      "height": 20
+     },
+     "index": 7,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Frame 6",
+     "rect": {
+      "x": 1183.82373046875,
+      "y": 83,
+      "width": 96,
+      "height": 20
+     },
+     "index": 8,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 1,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Footer Home",
+     "rect": {
+      "x": 160,
+      "y": 10519,
+      "width": 270.82373046875,
+      "height": 20
+     },
+     "index": 9,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Home",
+     "rect": {
+      "x": 1184,
+      "y": 10519,
+      "width": 96,
+      "height": 20
+     },
+     "index": 10,
+     "reactions": [
+      {
+       "action": "URL",
+       "srcPageIndex": 2,
+       "url": "https://www.linkedin.com/in/berthaclchan/",
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Pagination Next",
+     "rect": {
+      "x": 1095,
+      "y": 10344,
+      "width": 185,
+      "height": 32
+     },
+     "index": 11,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 3,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "1323:12490",
+   "groupIndex": 0,
+   "title": "Onboarding",
+   "image": "onboarding.png",
+   "index": 3,
+   "width": 1440,
+   "height": 12014,
+   "x": 3755,
+   "y": 1177,
+   "awidth": 1440,
+   "aheight": 12014,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Home",
+     "rect": {
+      "x": 160,
+      "y": 83,
+      "width": 270.82373046875,
+      "height": 20
+     },
+     "index": 12,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Frame 6",
+     "rect": {
+      "x": 1183.82373046875,
+      "y": 83,
+      "width": 96,
+      "height": 20
+     },
+     "index": 13,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 1,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Home",
+     "rect": {
+      "x": 1184,
+      "y": 11939,
+      "width": 96,
+      "height": 20
+     },
+     "index": 14,
+     "reactions": [
+      {
+       "action": "URL",
+       "srcPageIndex": 3,
+       "url": "https://www.linkedin.com/in/berthaclchan/",
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Pagination Previous",
+     "rect": {
+      "x": 150,
+      "y": 11720,
+      "width": 266,
+      "height": 32
+     },
+     "index": 15,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  }
+ ],
+ "startPageIndex": 0,
+ "totalImages": 4,
+ "fileKey": "portfolio",
+ "groups": [
+  {
+   "id": "404:90",
+   "index": 0,
+   "name": " 💻 Design V2",
+   "backColor": "#E5E5E5"
+  }
+ ]
+}
